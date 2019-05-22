c5e3468750d900860f3ea1adcf8895b835bc2803
Romain Bertrand <romain@brtd.me>
2019-05-22 09:34:56 +0100

    Simpler tests

--- src/__tests__/select-event.test.tsx
+++ src/__tests__/select-event.test.tsx
@@ -19,118 +19,118 @@ const renderForm = (select: React.ReactNode) => {
       {select}
     </form>
   );
-  const getForm = () => result.getByTestId("form");
-  const getInput = () => result.getByLabelText("Food");
-  return { ...result, getForm, getInput };
+  const form = result.getByTestId("form");
+  const input = result.getByLabelText("Food");
+  return { ...result, form, input };
 };
 
 describe("The select event helpers", () => {
   it("selects an option in a single-option input", async () => {
-    const { getForm, getInput } = renderForm(<Select {...defaultProps} />);
-    expect(getForm()).toHaveFormValues({ food: "" });
-    await selectEvent.select(getInput(), "Chocolate");
-    expect(getForm()).toHaveFormValues({ food: "chocolate" });
+    const { form, input } = renderForm(<Select {...defaultProps} />);
+    expect(form).toHaveFormValues({ food: "" });
+    await selectEvent.select(input, "Chocolate");
+    expect(form).toHaveFormValues({ food: "chocolate" });
   });
 
   it("selects several options in a multi-options input", async () => {
-    const { getForm, getInput } = renderForm(
+    const { form, input } = renderForm(
       <Select {...defaultProps} isMulti />
     );
-    expect(getForm()).toHaveFormValues({ food: "" });
+    expect(form).toHaveFormValues({ food: "" });
 
-    await selectEvent.select(getInput(), "Chocolate");
-    expect(getForm()).toHaveFormValues({ food: "chocolate" });
+    await selectEvent.select(input, "Chocolate");
+    expect(form).toHaveFormValues({ food: "chocolate" });
 
-    await selectEvent.select(getInput(), "Vanilla");
-    expect(getForm()).toHaveFormValues({ food: ["chocolate", "vanilla"] });
+    await selectEvent.select(input, "Vanilla");
+    expect(form).toHaveFormValues({ food: ["chocolate", "vanilla"] });
   });
 
   it("selects several options at the same time", async () => {
-    const { getForm, getInput } = renderForm(
+    const { form, input } = renderForm(
       <Select {...defaultProps} isMulti />
     );
-    expect(getForm()).toHaveFormValues({ food: "" });
-    await selectEvent.select(getInput(), ["Strawberry", "Mango"]);
-    expect(getForm()).toHaveFormValues({ food: ["strawberry", "mango"] });
+    expect(form).toHaveFormValues({ food: "" });
+    await selectEvent.select(input, ["Strawberry", "Mango"]);
+    expect(form).toHaveFormValues({ food: ["strawberry", "mango"] });
   });
 
   it("types in and add a new option", async () => {
-    const { getForm, getInput } = renderForm(<Creatable {...defaultProps} />);
-    expect(getForm()).toHaveFormValues({ food: "" });
-    await selectEvent.create(getInput(), "papaya");
-    expect(getForm()).toHaveFormValues({ food: "papaya" });
+    const { form, input } = renderForm(<Creatable {...defaultProps} />);
+    expect(form).toHaveFormValues({ food: "" });
+    await selectEvent.create(input, "papaya");
+    expect(form).toHaveFormValues({ food: "papaya" });
   });
 
   it("types in and add several options", async () => {
-    const { getForm, getInput } = renderForm(
+    const { form, input } = renderForm(
       <Creatable {...defaultProps} isMulti />
     );
-    expect(getForm()).toHaveFormValues({ food: "" });
-    selectEvent.create(getInput(), "papaya");
-    selectEvent.create(getInput(), "peanut butter");
-    expect(getForm()).toHaveFormValues({ food: ["papaya", "peanut butter"] });
+    expect(form).toHaveFormValues({ food: "" });
+    selectEvent.create(input, "papaya");
+    selectEvent.create(input, "peanut butter");
+    expect(form).toHaveFormValues({ food: ["papaya", "peanut butter"] });
   });
 
   it("clears the first item in a single-select dropdown", async () => {
-    const { getForm, getInput } = renderForm(
+    const { form, input } = renderForm(
       <Creatable {...defaultProps} isMulti defaultValue={OPTIONS[0]} />
     );
-    expect(getForm()).toHaveFormValues({ food: "chocolate" });
-    selectEvent.clearFirst(getInput());
+    expect(form).toHaveFormValues({ food: "chocolate" });
+    selectEvent.clearFirst(input);
     await wait(() => {
-      expect(getForm()).toHaveFormValues({ food: "" });
+      expect(form).toHaveFormValues({ food: "" });
     });
   });
 
   it("clears the first item in a multi-select dropdown", async () => {
-    const { getForm, getInput } = renderForm(
+    const { form, input } = renderForm(
       <Creatable
         {...defaultProps}
         isMulti
         defaultValue={[OPTIONS[0], OPTIONS[1], OPTIONS[2]]}
       />
     );
-    expect(getForm()).toHaveFormValues({
+    expect(form).toHaveFormValues({
       food: ["chocolate", "vanilla", "strawberry"]
     });
 
-    selectEvent.clearFirst(getInput());
+    selectEvent.clearFirst(input);
     await wait(() => {
-      expect(getForm()).toHaveFormValues({ food: ["vanilla", "strawberry"] });
+      expect(form).toHaveFormValues({ food: ["vanilla", "strawberry"] });
     });
 
-    selectEvent.clearFirst(getInput());
+    selectEvent.clearFirst(input);
     await wait(() => {
-      expect(getForm()).toHaveFormValues({ food: "strawberry" });
+      expect(form).toHaveFormValues({ food: "strawberry" });
     });
   });
 
   it("clears all items in a single-select dropdown", async () => {
-    const { getForm, getInput } = renderForm(
+    const { form, input } = renderForm(
       <Creatable {...defaultProps} isMulti defaultValue={OPTIONS[0]} />
     );
-    expect(getForm()).toHaveFormValues({ food: "chocolate" });
-    selectEvent.clearAll(getInput());
+    expect(form).toHaveFormValues({ food: "chocolate" });
+    selectEvent.clearAll(input);
     await wait(() => {
-      expect(getForm()).toHaveFormValues({ food: "" });
+      expect(form).toHaveFormValues({ food: "" });
     });
   });
 
   it("clears all items in a multi-select dropdown", async () => {
-    const { getForm, getInput } = renderForm(
+    const { form, input } = renderForm(
       <Creatable
         {...defaultProps}
         isMulti
         defaultValue={[OPTIONS[0], OPTIONS[1], OPTIONS[2]]}
       />
     );
-    expect(getForm()).toHaveFormValues({
+    expect(form).toHaveFormValues({
       food: ["chocolate", "vanilla", "strawberry"]
     });
 
-    selectEvent.clearAll(getInput());
+    selectEvent.clearAll(input);
     await wait(() => {
-      expect(getForm()).toHaveFormValues({ food: "" });
+      expect(form).toHaveFormValues({ food: "" });
     });
   });
 });
